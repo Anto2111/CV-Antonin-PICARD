91a06a4c29a4dcd4e4fe075933d048ec6edeeba4
Anto2111 <95379201+Anto2111@users.noreply.github.com>
2021-12-14 21:25:28 +0100

    Update btn-ci.js

--- btn java/btn-ci.js
+++ btn java/btn-ci.js
@@ -3,7 +3,7 @@ const btnci = document.querySelector('.btnci');
 btnci.addEventListener('click', () => {
 
     window.scrollTo({
-        top: 2450,
+        top: 2455,
         left: 0,
         behavior: "smooth"
     })
